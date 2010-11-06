4724ca1595246fe6078c6615062a027d817ae700
John S. Ryan <jtigger@infosysengr.com>
2010-11-06 13:57:44 -0700

    (Story 1) Added model validation for point estimations
    + Only values from the Fibonacci sequence (upto 21) are allowed.

--- code/model/story_card.rb
+++ code/model/story_card.rb
@@ -5,4 +5,13 @@
 class StoryCard
   attr_accessor :estimated_points
   attr_accessor :priority
+  
+  @@Acceptable_Point_Values = [1,3,5,8,13,21]
+  def estimated_points=(value)
+    if @@Acceptable_Point_Values.member? value
+      @estimated_points = value
+    else
+      raise "Invalid value for estimated_points.  Acceptable values are " + @@Acceptable_Point_Values.join(', ') + "."
+    end
+  end
 end
---
+++ unit-test/test_story_card.rb
@@ -0,0 +1,32 @@
+require "test/unit"
+
+require File.dirname(__FILE__) + "/../code/model/story_card.rb"
+
+class TestStoryCard < Test::Unit::TestCase
+  
+  def test_only_accepts_estimates_in_fibonacci_sequence_upto_21
+    story_card = StoryCard.new
+    
+    story_card.estimated_points = 1
+    assert_raise( RuntimeError ) { story_card.estimated_points = 2 }
+    story_card.estimated_points = 3
+    assert_raise( RuntimeError ) { story_card.estimated_points = 4 }
+    story_card.estimated_points = 5
+    assert_raise( RuntimeError ) { story_card.estimated_points = 6 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 7 }
+    story_card.estimated_points = 8
+    assert_raise( RuntimeError ) { story_card.estimated_points = 9 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 10 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 11 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 12 }
+    story_card.estimated_points = 13
+    assert_raise( RuntimeError ) { story_card.estimated_points = 14 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 15 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 16 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 17 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 18 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 19 }
+    assert_raise( RuntimeError ) { story_card.estimated_points = 20 }
+    story_card.estimated_points = 21
+  end
+end
