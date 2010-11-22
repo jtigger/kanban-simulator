727bc8c5f4708e83f3d903cad6f46e3df75f86b9
John S. Ryan <john@tacitknowlege.com>
2010-11-22 09:02:13 -0800

    (Story 2) SDLC configuration line is parsed.

--- code/model/config/configuration_parser.rb
+++ code/model/config/configuration_parser.rb
@@ -2,17 +2,30 @@
 # Author:: John S. Ryan (jtigger@infosysengr.com)
 class ConfigurationParser
   
+  Establish_Workflow_Pattern = /.*using the "(.*)" SDLC/i
+  
   # Parses a natural language configuration, building up a configuration plan
   # that can be applied to a Simulation.
   def parse(natural_language_config)
+    config_plan = [];
     
-    return [];
+    if Establish_Workflow_Pattern.match(natural_language_config)
+      workflow_name = Establish_Workflow_Pattern.match(natural_language_config)[0]
+      config_step = EstablishWorkflow.new(workflow_name)
+      config_plan << config_step
+    end
+    
+    return config_plan;
   end
 end
 
 # Configuration Commands:
 class EstablishWorkflow
   attr_accessor :workflow_name;
+  
+  def initialize(workflow_name)
+    self.workflow_name = workflow_name
+  end
   
   def configure(simulation)
     
--- unit-test/test_configuration.rb
+++ unit-test/test_configuration.rb
@@ -6,9 +6,22 @@ require File.dirname(__FILE__) + "/../code/model/config/configuration_parser.rb"
 
 # Author:: John S. Ryan (jtigger@infosysengr.com)
 class TestConfiguration < Test::Unit::TestCase
-  def test_parse_config_line
+  def test_parse_config_line_with_just_sdlc_specified
     parser = ConfigurationParser.new
+    config_line = 'We are using the "Kanban" SDLC.'
+
+    config_plan = parser.parse config_line
+    
+    assert_equal(1, config_plan.size)
+    assert(config_plan[0].kind_of?(EstablishWorkflow))
+  end
+
+  # Future tests:
+  # test_parse_config_line_with_sdlc_and_wip_limits
+  
+  def test_configure_simulation
     simulation = Simulation.new
+    parser = ConfigurationParser.new
     
     config_line = 'We are using the "Kanban" SDLC where the "WIP limit" for "In Analysis" is 3; and the "WIP limit" for "In Dev" is 4; and the "WIP limit" for "In Test" is 2.'
 
