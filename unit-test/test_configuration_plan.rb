9f5e15cb9a46a51ed1640eb9f023e68469c35989
John S. Ryan <john@tacitknowlege.com>
2010-11-24 04:35:18 -0800

    (minor) Added Sample SDLC in place of Kanban for use in tests.
    - I didn't want tests to be dependent on a specific SDLC, just on
      _some_ SDLC. (better isolation)
    - Frees Kanban to have a life of its own without worry of breaking unit
      tests. (more maintainable)

--- unit-test/test_configuration_plan.rb
+++ unit-test/test_configuration_plan.rb
@@ -4,6 +4,18 @@ require File.dirname(__FILE__) + "/../code/app/simulation.rb"
 require File.dirname(__FILE__) + "/../code/model/config/configuration_parser.rb"
 require File.dirname(__FILE__) + "/../code/model/sdlc.rb"
 
+class SDLC
+  def SDLC.TestSDLC
+    workflow = []
+    workflow << KanbanProcessStep.new("In Analysis", 3)
+    workflow << KanbanProcessStep.new("In Dev", 3)
+    workflow << KanbanProcessStep.new("In Test", 3)
+    workflow << KanbanProcessStep.new("Done", nil)
+    
+    workflow
+  end
+end
+
 # Author:: John S. Ryan (jtigger@infosysengr.com)
 class TestConfigurationPlan < Test::Unit::TestCase
 
@@ -18,10 +30,10 @@ class TestConfigurationPlan < Test::Unit::TestCase
   end
 
   def test_plan_correctly_seeds_workflow
-    config_plan = @parser.parse('Using the "Kanban" SDLC.')
+    config_plan = @parser.parse('Using the "TestSDLC" SDLC.')
     @simulation.configure(config_plan)
     
-    assert_equal(SDLC.Kanban.size, @simulation.workflow.size)
+    assert_equal(SDLC.TestSDLC.size, @simulation.workflow.size)
   end
 
   def test_plan_correctly_errors_on_unknown_workflow
