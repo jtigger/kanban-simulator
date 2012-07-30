1bcba8122e680408a6aa9c075106ebeedea11f98
John S. Ryan <jtigger@bigvisible.com>
2012-07-30 07:14:22 -0700

    (Fix) Snap!  Forgot to add "IterationParameterTest" to the suite.

--- kanbansimulator/src/test/java/com/bigvisible/kanbansimulatortester/suites/AllUnitTests.java
+++ kanbansimulator/src/test/java/com/bigvisible/kanbansimulatortester/suites/AllUnitTests.java
@@ -4,6 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.bigvisible.kanbansimulator.IterationParameter;
+import com.bigvisible.kanbansimulatortester.core.unit.IterationParameterTest;
 import com.bigvisible.kanbansimulatortester.core.unit.IterationResultTest;
 import com.bigvisible.kanbansimulatortester.core.unit.StimulatorTest;
 
@@ -11,6 +13,6 @@ import com.bigvisible.kanbansimulatortester.core.unit.StimulatorTest;
 @SuiteClasses({ IterationResultTest.When_a_typical_iteration_is_run.class,
         IterationResultTest.When_the_next_iteration_is_generated.class,
         IterationResultTest.Iteration_Results_can_be_serialized_to_and_from_CSV.class,
-        IterationResultTest.Uncategorized.class, StimulatorTest.class })
+        IterationResultTest.Uncategorized.class, StimulatorTest.class, IterationParameterTest.class })
 public class AllUnitTests {
 }
