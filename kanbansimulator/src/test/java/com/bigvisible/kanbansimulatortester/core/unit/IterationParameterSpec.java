36cce25c1c611ad82e37718241a130b1bbf7add5
John S. Ryan <jtigger@bigvisible.com>
2012-09-12 07:51:40 -0700

    Removed unnecessary test case.
    We're not going to spend time specifying behavior we won't use, at this
    time.  This kind of testing would be part of a "hardening" effort.

--- kanbansimulator/src/test/java/com/bigvisible/kanbansimulatortester/core/unit/IterationParameterSpec.java
+++ kanbansimulator/src/test/java/com/bigvisible/kanbansimulatortester/core/unit/IterationParameterSpec.java
@@ -29,10 +29,4 @@ public class IterationParameterSpec {
         assertNotNull("Expected to find parameter for \"First Step\" in IterationParameter, but not found.", iterationParameter.getParameterForStep("First Step"));
         assertNotNull("Expected to find parameter for \"Second Step\" in IterationParameter, but not found.",iterationParameter.getParameterForStep("Second Step"));
     }
-    
-    // TODO-NEXT: GIVEN_a_IterationParameter_with_a_WorkflowStepParameter_configured_WHEN_a_WorkflowStepParameter_is_set_for_the_same_WorkflowStep_THEN_the_new_parameter_replaces_the_old_one
-    public void GIVEN_a_IterationParameter_with_a_WorkflowStepParameter_configured_WHEN_a_WorkflowStepParameter_is_set_for_the_same_WorkflowStep_THEN_the_new_parameter_replaces_the_old_one()
-    {
-        
-    }
 }
