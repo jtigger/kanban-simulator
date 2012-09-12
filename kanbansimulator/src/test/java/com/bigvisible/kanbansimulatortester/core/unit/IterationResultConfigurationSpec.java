034d80f540b1aeab33f602bc4c0348a3ae3b1276
John S. Ryan <jtigger@bigvisible.com>
2012-09-12 07:48:42 -0700

    Fleshed-out IterationResult unit spec
    + part of an effort to "right" the mistake of focusing too much on
    Simulator as the point of testing.  With these specs, its much more
    clear that a) these are the possible configuration operations and b)
    that IterationResult itself is responsible for putting these
    configurations into effect.

--- kanbansimulator/src/test/java/com/bigvisible/kanbansimulatortester/core/unit/IterationResultConfigurationSpec.java
+++ kanbansimulator/src/test/java/com/bigvisible/kanbansimulatortester/core/unit/IterationResultConfigurationSpec.java
@@ -15,19 +15,6 @@ import com.bigvisible.kanbansimulator.IterationResult;
 
 public class IterationResultConfigurationSpec {
 
-    // TODO-NEXT: enumerate other aspects of the existing contract of "IterationResult.configure()".
-    
-    @Test
-    public void GIVEN_configuration_says_to_remove_a_workflow_step_WHEN_the_iteration_is_configured_THEN_the_iteration_does_not_have_that_workflow_step() throws Exception {
-        IterationResult iteration = new IterationResult();
-        List<IterationParameter> params = new LinkedList<IterationParameter>();
-        params.add(startingAt(1).forStep(named("WebDev").remove()));
-        
-        iteration.configure(params);
-        
-        assertThat("WebDev", not(isIn(iteration.workflowStepNames())));
-    }
-    
     @Test
     public void GIVEN_configuration_sets_batch_size_WHEN_the_iteration_is_configured_THEN_the_batch_size_of_the_iteration_is_that_value() throws Exception {
         IterationResult iteration = new IterationResult();
@@ -39,5 +26,46 @@ public class IterationResultConfigurationSpec {
         iteration.configure(params);
         
         assertThat(iteration.getBatchSize(), is(configuredBatchSize));
+    }
+
+    @Test
+    public void GIVEN_configuration_sets_capacity_for_a_workflow_step_WHEN_the_iteration_is_configured_THEN_the_capacity_of_that_workflow_step_is_that_value() throws Exception {
+        IterationResult iteration = new IterationResult();
+        List<IterationParameter> params = new LinkedList<IterationParameter>();
+        
+        String someWorkflowStep = "Dev";
+        int configuredCapacity = UnitTestHelper.anyReasonableNumber();
+        
+        params.add(startingAt(1).forStep(named(someWorkflowStep)).setCapacity(configuredCapacity));
+        iteration.configure(params);
+        
+        assertThat(iteration.getCapacity(someWorkflowStep), is(configuredCapacity));
+        
+    }
+
+    @Test
+    public void GIVEN_two_configuration_set_the_same_parameter_WHEN_the_iteration_is_configured_THEN_the_last_configuration_wins() throws Exception {
+        IterationResult iteration = new IterationResult();
+        List<IterationParameter> params = new LinkedList<IterationParameter>();
+        int firstConfiguredBatchSize = 10;
+        int secondConfiguredBatchSize = 20;
+
+        params.add(startingAt(1).setBatchSize(firstConfiguredBatchSize));
+        params.add(startingAt(1).setBatchSize(secondConfiguredBatchSize));
+        
+        iteration.configure(params);
+        
+        assertThat(iteration.getBatchSize(), is(secondConfiguredBatchSize));
+    }
+    
+    @Test
+    public void GIVEN_configuration_says_to_remove_a_workflow_step_WHEN_the_iteration_is_configured_THEN_the_iteration_does_not_have_that_workflow_step() throws Exception {
+        IterationResult iteration = new IterationResult();
+        List<IterationParameter> params = new LinkedList<IterationParameter>();
+        params.add(startingAt(1).forStep(named("WebDev").remove()));
+        
+        iteration.configure(params);
+        
+        assertThat("WebDev", not(isIn(iteration.workflowStepNames())));
     }
 }
